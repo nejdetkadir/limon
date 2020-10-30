b79b9f9944e07ca4dc0afbc91cbd8742d74c6d9e
nejdetkadir <nejdetkadir.550@gmail.com>
2020-10-30 22:37:11 +0300

    Create UI of celebrate component

--- src/components/Celebrate.vue
+++ src/components/Celebrate.vue
@@ -1,15 +1,52 @@
 <template>
-  <div>
-    celebrate
+  <div class="container">
+    <h1>You know right :)</h1>
+    <button @click="newGame()">NEW GAME</button>
   </div>
 </template>
 
 <script>
 export default {
-name: "Celebrate"
+  methods: {
+    newGame() {
+
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+.container{
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.container h1{
+  text-align: center;
+  color: darkgreen;
+  font-size: 333%;
+  opacity: .5;
+}
+.container button{
+  width: 200px;
+  background-color: #fff;
+  color: darkred;
+  border: 1px solid darkgreen;
+  height: 50px;
+  font-size: 15px;
+  border-radius: 5px;
+  outline: none;
+  transition: all .5s;
+}
+.container button:hover{
+  background-color: darkgreen;
+  color: #fff;
+  transition: all .5s;
+}
 </style>
